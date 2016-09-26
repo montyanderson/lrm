bd5bc7ee2e8f0deb8d0a166124caeb21e6ed016e
Monty Anderson <monty.linux@gmail.com>
2016-09-26 11:38:56 +0100

    submit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = new Sequelize(argv.db, argv.user, argv.password);
 
 const Resource = db.define("resource", {
 	title: Sequelize.STRING,
+	url: Sequelize.STRING,
 	date: Sequelize.DATE
 });
 
@@ -17,11 +18,20 @@ app.get("/search", (req, res) => {
 	Resource.findAll({
 		where: {
 			title: {
-				$like: "%" + req.query.q + "%"
+				$like: "%" + req.query.q.replace(new RegExp(" ", "g"), "%") + "%"
 			}
 		}
 	}).then(resources => {
 		res.end(JSON.stringify(resources));
+	});
+});
+
+app.get("/submit", (req, res) => {
+	Resource.create({
+		title: req.query.title,
+		url: req.query.url
+	}).then(() => {
+		res.end();
 	});
 });
 
